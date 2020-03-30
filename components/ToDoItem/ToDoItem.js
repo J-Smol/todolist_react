43177e36f4ca64446671b917309d1d28a28851ec
Jane <jn.sm.new@gmail.com>
2020-03-30 15:25:29 +0300

    Add files via upload

---
+++ components/ToDoItem/ToDoItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './ToDoItem.css';
+
+const ToDoItem = props => {
+    const resolvedClass = {
+        textDecoration : 'line-through',
+        fontWeight: 200
+    }
+    return (
+        <div className="todo__item">
+            <div className="input__wrapper">
+                <input 
+                    type="checkbox"
+                    defaultChecked={props.completed}
+                    onChange={props.handleChange}
+                />
+            </div>
+            <div className="description__wrapper">
+                <p 
+                    style={props.completed == true ? resolvedClass : {}} 
+                >
+                    {props.description}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ToDoItem;
